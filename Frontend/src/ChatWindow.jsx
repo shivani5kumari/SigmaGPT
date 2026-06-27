22723c65ec8f52b7d7373816b5174f70409c3423
Shivani kumari <shivanikumari3968@gmail.com>
2026-06-27 17:38:55 +0530

    Add Project Files

--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -40,7 +40,7 @@ function ChatWindow() {
         };
 
         try {
-            const response = await fetch("http://localhost:8080/api/chat", options);
+            const response = await fetch("https://sigmagpt-1-pb0i.onrender.com/api/chat", options);
             const res = await response.json();
             console.log(res);
             setReply(res.reply);
